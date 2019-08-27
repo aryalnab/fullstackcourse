ef28790f70305ea5f6c543d9fcbb25b56be9b3ef
aryalnab <nabin.aryal@outlook.com>
2019-08-27 16:23:49 +0300

    Part 1 Javascript

---
+++ Part1/course-info/src/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from './App.js'
+import './index.css';
+
+const Header = (props) => {
+ return (
+     <div> 
+        <h1>{props.course}</h1>    
+     </div>
+ )   
+}
+
+const Content = (props) => {
+    return (
+    <div> 
+        <p>
+          {props.part1}
+        </p>
+        <p>
+          {props.part2}
+        </p>
+        <p>
+          {props.part3}
+        </p>
+     </div>
+    )   
+   }
+
+   
+const Total = (props) => {
+    return (
+        <div> 
+          <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>  
+        </div>
+    )   
+   }
+   const App = () => {
+    const course = 'Half Stack application development'
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+
+
+
+   
+    return (
+      <div>
+        <Header course = {course} />
+        <Content 
+        part1 = {parts[0].name}
+        part2 = {parts[1].name}
+        part3 = {parts[2].name}/>
+        <Total exercises1 = {parts[0].exercises} 
+        exercises2 = {parts[1].exercises}
+        exercises3 = {parts[2].exercises}
+        />
+      </div>
+    )
+  }
+  
+ReactDOM.render(<App />, document.getElementById('root'));
+
